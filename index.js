ac48cfe9962faa8294d64d822b9b654bccc37a59
Michael Cobacango <michaelpm63@gmail.com>
2022-04-06 13:47:48 -0500

    hola mundo

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 4000;
 app.listen(port);
 app.get("/", (req, res) => {
-    res.send("Pagina de Inicio");
-    res.send("Hola Mundo");
+    res.send("Pagina de Inicio Hola Mundo");
 });
 console.log(`Listen on Port ${port}`);
