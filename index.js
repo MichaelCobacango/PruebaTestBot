1cb5b24d1be854ff0f74f24b97aa3e5203aebce8
Michael <michaelpm63@gmail.com>
2022-04-09 12:49:17 -0500

    Cambios

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port);
+
+app.send("/",(req,res)=>{
+    res.send("Hello Woeld");
+})
 console.log(`App listen in the port ${port}`);
