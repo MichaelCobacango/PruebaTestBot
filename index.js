90d04a591004b5103e9bd3bd6ae2a2fb7dd5f812
Michael <michaelpm63@gmail.com>
2022-04-08 21:46:37 -0500

    Hello World and add file vercel

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 console.log(`App listen in the port ${port}`);
